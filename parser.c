ed1b3ddb338eed8e046b8fe21bfed3e137f15060
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-06-06 12:00:19 +0300

    Add swap opcode implementation

--- parser.c
+++ parser.c
@@ -20,6 +20,8 @@ else if (strcmp(opcode, "pint") == 0)
 	pint(stack, line_number);
 else if (strcmp(opcode, "pop") == 0)
 	pop(stack, line_number);
+else if (strcmp(opcode, "swap") == 0)
+	swap(stack, line_number);
     else
     {
         fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
--- push_pall.c
+++ push_pall.c
@@ -131,3 +131,32 @@ void add(stack_t **stack, unsigned int line_number)
 
 	free(first);
 }
+/**
+ * swap - swaps top two elements of stack
+ * @stack: pointer to stack
+ * @line_number: line number
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first;
+	stack_t *second;
+
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	first = *stack;
+	second = first->next;
+
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+
+	*stack = second;
+}
